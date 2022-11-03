43151ca712801298db8ecc90df80dcc73bdd16f8
declanhiller <declan.hiller@gmail.com>
2022-11-03 10:34:07 -0700

    first commit

---
+++ Assets/Scripts/Dialogue/CharacterController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private string name;
+
+    private float affectionMeter;
+
+    
+    //load json into a file
+    
+    //load affection increase, load affection decrease
+    //load dialogue...
+    //load options...
+        //for each option
+        //load affection increase,
+        //load affection decrease
+
+
+    private void LoadDialogue()
+    {
+        
+    }
+    
+    
+}
---
+++ Assets/Scripts/Dialogue/DialogueChunk.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueChunk
+{
+    public List<String> dialogue;
+}
